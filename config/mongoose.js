ff76b9ff3c9c91c90181b819f40f753601155d9d
Mukesh <mukeshbisht7590@gmail.com>
2022-10-18 12:53:12 +0530

    post create-delete profile-update flashMessage

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,30 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-   try {
-      // console.log("dfsfsfs ",process.env.MONGO_URI)
-      const conn = await mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(
+   `mongodb+srv://Admin123456:Admin123456@socialmediacluster.fivuvjj.mongodb.net/Codeial?retryWrites=true&w=majority`
+);
 
-      console.log(`                                 MongoDB Connection Established          
-      
-      `);
-   } catch (error) {
-      console.log(error);
-      process.exit(1);
-   }
-};
-module.exports = connectDB;
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once("open", function () {
+   console.log("connected to the database :: MongoDB");
+});
+
+module.exports = db;
+
+// const connectDB = async () => {
+//    try {
+//       // console.log("dfsfsfs ",process.env.MONGO_URI)
+//       const conn = await mongoose.connect(process.env.MONGO_URI);
+
+//       console.log(`                                 MongoDB Connection Established
+
+//       `);
+//    } catch (error) {
+//       console.log(error);
+//       process.exit(1);
+//    }
+// };
+// module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,19 +1,82 @@
-const app = require("express")()
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
 const dotenv = require("dotenv");
-const connectDB = require("./config/mongoose");
-
+const expressLayouts = require("express-ejs-layouts");
 dotenv.config();
-connectDB();
+const db = require("./config/mongoose");
+// const connectDB = require("./config/mongoose");
+const flash = require("connect-flash");
+const customWare = require("./config/middleware");
+
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo");
+const sassMiddleware = require("node-sass-middleware");
+
+app.use(
+   sassMiddleware({
+      src: "./assets/scss",
+      dest: "./assets/css",
+      debug: "true",
+      outputStyle: "expanded",
+      prefix: "/css",
+   })
+);
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(express.static("./assets"));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+// set up the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// mongo store is used to store the session cookie in the db
+app.use(
+   session({
+      name: "codeial",
+      // TODO change the secret before deployment in production mode
+      secret: "blahsomething",
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+         maxAge: 1000 * 60 * 100,
+      },
+      store: MongoStore.create(
+         {
+            mongoUrl: process.env.MONGO_URI,
+            autoRemove: "disabled",
+         },
+         function (err) {
+            console.log(err || "connect-mongodb setup ok");
+         }
+      ),
+   })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customWare.setFlash);
+
+// use express router
+app.use("/", require("./routes"));
 
 app.listen(process.env.PORT, (error) => {
-    if (error) {
-       console.log("error in the port");
-    }
-    console.log(
-       `     
- 
-       ******************* HEY! SERVER IS RUNNING on Link :  http://localhost:${process.env.PORT} **********************
-       
-       `
-    );
- });
+   if (error) {
+      console.log("error in the port");
+   }
+   console.log(
+      `\nHEY! SERVER IS RUNNING on Link :  http://localhost:${process.env.PORT}\n`
+   );
+});
